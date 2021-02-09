27ae971c12a825655423baf9383996ec401c4f29
Alberto Marchetti <cmaster11@gmail.com>
2021-02-09 22:57:01 +0200

    wip

--- attiny/attiny.ino
+++ attiny/attiny.ino
@@ -1,8 +1,21 @@
 #include <TinyWireS.h>
+#include <NewPing.h>
+
+#define SONAR_TRIGGER_PIN 5
+#define SONAR_ECHO_PIN 4
+
+#define SONAR_MAX_DISTANCE 250 // cm
 
 #define I2C_SLAVE_ADDRESS 0x4 // Address of the slave
 
+// Operation to execute, sent from rpi
 byte receive_op;
+
+// NewPing setup of pins and maximum distance.
+NewPing sonar(SONAR_TRIGGER_PIN, SONAR_ECHO_PIN, SONAR_MAX_DISTANCE);
+
+// In any case, cannot be > SONAR_MAX_DISTANCE
+byte sonar_distance;
 
 /*
    Operations:
@@ -22,8 +35,17 @@ void setup()
   digitalWrite(1, HIGH);
 }
 
+bool sonar_run = false;
+
 void loop()
 {
+  /**
+     This is the only way we can detect stop condition (http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&p=984716&sid=82e9dc7299a8243b86cf7969dd41b5b5#984716)
+     it needs to be called in a very tight loop in order not to miss any (REMINDER: Do *not* use delay() anywhere, use tws_delay() instead).
+     It will call the function registered via TinyWireS.onReceive(); if there is data in the buffer on stop.
+  */
+  TinyWireS_stop_check();
+  
   switch (receive_op) {
     case 0:
       digitalWrite(1, LOW);
@@ -31,22 +53,25 @@ void loop()
     case 1:
       digitalWrite(1, HIGH);
       break;
+    case 2:
+      sonar_run = true;
+      break;
   }
 
-  /**
-     This is the only way we can detect stop condition (http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&p=984716&sid=82e9dc7299a8243b86cf7969dd41b5b5#984716)
-     it needs to be called in a very tight loop in order not to miss any (REMINDER: Do *not* use delay() anywhere, use tws_delay() instead).
-     It will call the function registered via TinyWireS.onReceive(); if there is data in the buffer on stop.
-  */
-  TinyWireS_stop_check();
+  // Force reset
+  receive_op = -1;
+
+  if (sonar_run) {
+    sonar_run = false;
+    sonar_distance = sonar.ping_cm();
+  }
 }
 
-int i = 0;
 // Gets called when the ATtiny receives an i2c request
 void requestEvent()
 {
-  TinyWireS.send(i);
-  i++;
+  // In any case, cannot be > SONAR_MAX_DISTANCE  
+  TinyWireS.send(sonar_distance);
 }
 
 /**
--- rpi/main.go
+++ rpi/main.go
@@ -16,37 +16,46 @@ func main() {
 	// Free I2C connection on exit
 	defer conn.Close()
 
+	go loopRead(conn)
+
 	loop(conn)
 }
 
+func loopRead(conn *i2c.Options) {
+	for {
+
+		{
+			// Read sonar value
+			bytes := []byte{0, 0}
+			numRead, err := conn.ReadBytes(bytes)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("Read %d bytes\n", numRead)
+			if len(bytes) > 0 {
+				log.Printf("Value: %d", bytes[0])
+			}
+		}
+
+		time.Sleep(250 * time.Millisecond)
+
+	}
+}
+
 func loop(conn *i2c.Options) {
-	on := false
 
 	for {
 
-		// Alternate LED
-
-		if !on {
-			log.Println("Turning ON")
-
-			_, err := conn.WriteBytes([]byte{0x1})
+		{
+			// Trigger sonar read
+			_, err := conn.WriteBytes([]byte{0x2})
 			if err != nil {
 				log.Fatal(err)
 			}
-
-			on = true
-		} else {
-			log.Println("Turning OFF")
-
-			_, err := conn.WriteBytes([]byte{0x0})
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			on = false
 		}
 
-		time.Sleep(2 * time.Second)
+		time.Sleep(500 * time.Millisecond)
 
 	}
 }
