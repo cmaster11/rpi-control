1764a405968c1c56b06ae49f7bee4c242515d949
Alberto Marchetti <cmaster11@gmail.com>
2021-02-18 07:47:50 +0200

    wip

--- scripts/c11hurr-get-gpu-temp.sh
+++ scripts/c11hurr-get-gpu-temp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -sS "http://192.168.1.105:10888/data.json" | jq -r '.Children[0].Children | .[] | select(.Text == "NVIDIA GeForce RTX 3090").Children | .[] | select(.Text == "Temperatures").Children[0].Value' | sed -Ee 's/^([0-9]+),.+$/\1/'
+curl -sS "http://192.168.1.105:10888/data.json" | jq -r '.Children[0].Children | .[] | select(.Text == "NVIDIA GeForce RTX 3090").Children | .[] | select(.Text == "Temperatures").Children[0].Value' | sed -Ee 's/^([0-9]+),.+$/\1/' | tr -d '\n'
