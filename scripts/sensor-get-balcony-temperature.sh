32ef60f128d2e6f1d1948b70e61003571ad3b360
Alberto Marchetti <cmaster11@gmail.com>
2021-02-19 22:13:24 +0200

    wip

--- scripts/sensor-get-balcony-temperature.sh
+++ scripts/sensor-get-balcony-temperature.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 temperature_sensor=$(curl -sS "http://192.168.1.39/api/958DAECBBC/sensors" | jq -r -M 'to_entries | .[] | select(.value.state.temperature != null) | .value.state.temperature')
-temperature_c=$(bc <<< "scale=2; $temperature_sensor/100")
+temperature_c=$(bc <<< "scale=1; $temperature_sensor/100")
 
 echo "$temperature_c"
